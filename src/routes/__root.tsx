4e9eabe6199e44db93028c6f98115b26f402ccb0
Mike Godfrinne <mike.godfrinne@gmail.com>
2026-03-03 05:45:32 +0100

    First commit with Tanstack Router Setup

---
+++ src/routes/__root.tsx
@@ -0,0 +1,13 @@
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+
+export const Route = createRootRoute({
+    component: () => (
+        <>
+            <nav>
+                <Link to='/'>Home</Link>
+                <Link to='/auth'>Login</Link>
+            </nav>
+            <Outlet />
+        </>
+    )
+})
